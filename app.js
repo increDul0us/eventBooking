7acb859295379666f8aeb6cdf6f37e3a1a04ca25
increDul0us <incredulous97@gmail.com>
2019-06-09 16:41:43 +0100

    fixed cors

--- app.js
+++ app.js
@@ -3,24 +3,26 @@ const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 
 const graphQlSchema = require('./graphql/schema/index')
 const graphQlResolvers = require('./graphql/resolvers/index')
 const isAuth = require('./middleware/is-auth')
 
 const app = express()
+app.use(cors())
 
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200)
-  }
-  next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS')
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200)
+//   }
+//   next()
+// })
 
 app.use(isAuth)
 
@@ -35,7 +37,7 @@ app.use(
 
 mongoose
   .connect(
-    `mongodb+srv://incredulous:incredulous@cluster0-jz2yt.mongodb.net/test?retryWrites=true&w=majority`
+    'mongodb+srv://incredulous:incredulous@cluster0-jz2yt.mongodb.net/test?retryWrites=true&w=majority'
   )
   .then(() => {
     console.log('MongoDB Connected')
